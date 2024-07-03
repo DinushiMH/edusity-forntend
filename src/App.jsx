de40924584f476d171816a6d1af59b641eca7acb
DinushiMH <dinushiherath98@gmail.com>
2024-07-03 13:37:59 -0700

    completed videoplay commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
@@ -11,6 +11,10 @@ import Footer from './Components/Footer/Footer'
 import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
+
+
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar/>
@@ -18,7 +22,7 @@ const App = () => {
       <div className="container">
         <Title subTitle='OUR PROGRAM' title='What We Offer'/>
         <Programs/>
-        <About/>
+        <About setPlayState={setPlayState}/>
         <Title subTitle='Gallery' title='Campus Photos'/>
         <Campus/>
         <Title subTitle='TESTIMONIALS' title='What Student Says'/>
@@ -27,7 +31,7 @@ const App = () => {
         <Contact/>
         <Footer/>
       </div>
-      <VideoPlayer/>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import './VideoPlayer.css'
-import video from '../../assets/college-video.mp4'
+import React, { useRef } from 'react';
+import './VideoPlayer.css';
+import video from '../../assets/college-video.mp4';
 
-const VideoPlayer = () => {
+const VideoPlayer = ({playState, setPlayState }) => {
+
+  const player = useRef(null);
+
+  const closePlayer = (e)=>{
+    if(e.target === player.current){
+      setPlayState(false);ට
+    }
+  }
+
   return (
-    <div className='video-player'>
-        <video src={video} autoPlay muted controls></video>
-      
+    <div className={`video-player ${playState ? '' : 'hide'}`} ref={player} onClick={closePlayer}>
+      <video src={video} autoPlay muted controls></video>
     </div>
-  )
-}
+  );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
