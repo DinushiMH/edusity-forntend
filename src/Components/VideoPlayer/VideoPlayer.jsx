cc7eadc34545130ddc44e98eacee2af19f1f4f17
DinushiMH <dinushiherath98@gmail.com>
2024-07-03 00:10:34 -0700

    third commit

---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './VideoPlayer.css'
+import video from '../../assets/college-video.mp4'
+
+const VideoPlayer = () => {
+  return (
+    <div className='video-player'>
+        <video src={video} autoPlay muted controls></video>
+      
+    </div>
+  )
+}
+
+export default VideoPlayer
